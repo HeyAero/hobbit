a6761d3966d9081ee14329ca07f091b4feca7aa3
HeyAero <aeroyts@icloud.com>
2021-04-07 08:51:31 +0100

    remove db/migration files and moved them into one sql file

--- api/dbConfig/dev_seeds.sql
+++ api/dbConfig/dev_seeds.sql
@@ -1,3 +1,25 @@
+DROP TABLE IF EXISTS users;
+
+CREATE TABLE users (
+  id serial PRIMARY KEY,
+  username VARCHAR(255) NOT NULL,
+  password_digest VARCHAR(500) NOT NULL
+)
+
+DROP TABLE IF EXISTS habits;
+
+CREATE TABLE habits (
+  id serial PRIMARY KEY,
+  name VARCHAR(255) NOT NULL,
+  habit_desc VARCHAR(255),
+  frequency VARCHAR(255),
+  streak_track INT NOT NULL,
+  streak_start VARCHAR(255) NOT NULL,
+  streak_end VARCHAR(255) NOT NULL,
+  user_id INT NOT NULL,
+  FOREIGN KEY (user_id) REFERENCES users(id)
+)
+
 INSERT INTO users(username, password_digest)
 VALUES
   ('Aaron', 'tu9ibtoi4tbh2hhuet'),
--- db/migrations/1_create_users.sql
+++
@@ -1,7 +0,0 @@
-DROP TABLE IF EXISTS users;
-
-CREATE TABLE users (
-  id serial PRIMARY KEY,
-  username VARCHAR(255) NOT NULL,
-  password_digest VARCHAR(500) NOT NULL
-)
